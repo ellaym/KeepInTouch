781d900af9114d1d3d9f9c3325d09d1c79f97be0
Ellay Milstein <ellaym@mail.tau.ac.il>
2024-02-16 22:50:55 +0200

    Fixed db handler to handle async operations properly

--- src/db_handler.js
+++ src/db_handler.js
@@ -1,32 +1,34 @@
 const sqlite3 = require("sqlite3").verbose();
 
 function load_db(db_path, sql_query) {
-  let db = new sqlite3.Database(db_path, sqlite3.OPEN_READONLY, (err) => {
-    if (err) {
-      console.error(err.message);
-    }
-    console.log("Connected to the database.");
-  });
+  return new Promise((resolve, reject) => {
+    let db = new sqlite3.Database(db_path, sqlite3.OPEN_READONLY, (err) => {
+      if (err) {
+        console.error(err.message);
+        reject(err);
+      }
+      console.log("Connected to the database.");
+    });
 
-  let output_rows = [];
+    let output_rows = [];
 
-  db.all(sql_query, [], (err, rows) => {
-    if (err) {
-      throw err;
-    }
-    rows.forEach((row) => {
-      output_rows.push(row);
+    db.all(sql_query, [], (err, rows) => {
+      if (err) {
+        reject(err);
+      }
+      rows.forEach((row) => {
+        output_rows.push(row);
+      });
+      resolve(output_rows);
+    });
+
+    db.close((err) => {
+      if (err) {
+        console.error(err.message);
+      }
+      console.log("Close the database connection.");
     });
   });
-
-  db.close((err) => {
-    if (err) {
-      console.error(err.message);
-    }
-    console.log("Close the database connection.");
-  });
-
-  return output_rows;
 }
 
 module.exports = load_db;
