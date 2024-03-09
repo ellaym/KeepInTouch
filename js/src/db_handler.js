3dba41679414771323dfe86f590beb37b1a6b534
Ellay Milstein <ellaym@mail.tau.ac.il>
2024-03-09 17:43:22 +0200

    Updated db_handler for json load and renamed db handler

--- js/src/db_handler.js
+++ js/src/db_handler.js
@@ -1,6 +1,7 @@
 const sqlite3 = require("sqlite3").verbose();
+const fs = require("fs");
 
-function load_db(db_path, sql_query) {
+function loadSQLDatabase(db_path, sql_query) {
   return new Promise((resolve, reject) => {
     const db = new sqlite3.Database(db_path, sqlite3.OPEN_READONLY, (err) => {
       if (err) {
@@ -26,4 +27,23 @@ function load_db(db_path, sql_query) {
   });
 }
 
-module.exports = load_db;
+function loadJsonFile(filePath) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, "utf8", (err, data) => {
+      if (err) {
+        console.error("Error reading the file:", err);
+        reject(err); // Reject the promise on error
+      } else {
+        try {
+          const jsonObj = JSON.parse(data);
+          resolve(jsonObj); // Resolve the promise with the JSON object
+        } catch (parseError) {
+          console.error("Error parsing JSON:", parseError);
+          reject(parseError); // Reject the promise on parsing error
+        }
+      }
+    });
+  });
+}
+
+module.exports = { loadSQLDatabase, loadJsonFile };
